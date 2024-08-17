bbfc2ce4304aa7e555e1dc7d168fddb801ce0d8e
Shyam Menon <shyam.menon@hp.com>
2024-08-17 08:08:41 +0530

    Modifications in Agent to invoke a kernel function

---
+++ SemanticKernelSamples/SK_Agents/EmailPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+public class EmailPlugin
+{
+    [KernelFunction("send_email")]
+    [Description("Sends an email to a recipient.")]
+    public async Task SendEmailAsync(
+        Kernel kernel,       
+        string subject,
+        string body
+    )
+    {
+        // Add logic to send an email using the recipientEmails, subject, and body
+        // For now, we'll just print out a success message to the console
+        Console.WriteLine(subject);
+        Console.WriteLine(body);
+        Console.WriteLine("Email sent!");
+    }
+}
--- SemanticKernelSamples/SK_Agents/Program.cs
+++ SemanticKernelSamples/SK_Agents/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Agents.OpenAI;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 // Initialize the Semantic Kernel
 var builder = Kernel.CreateBuilder();
-
-//builder.Plugins.AddFromType<EmailPlugin>();
+builder.Plugins.AddFromType<EmailPlugin>();
+var kernel = builder.Build();
 
 // Get the API key from environment variables
 var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
@@ -23,8 +24,11 @@ modelId: "gpt-4o-mini",
 apiKey: apiKey // Your OpenAI API key
 );
 
-var kernel = builder.Build();
-
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
 
 // Optional: Specify an endpoint if you have one, otherwise it can be null
 Uri? endpoint = null; 
@@ -42,9 +46,9 @@ var client = provider.Client;
 // Create an instance of OpenAIAssistantDefinition
 var assistantDefinition = new OpenAIAssistantDefinition("gpt-4o-mini")
 {
-    Description = "This is a sample assistant",
-    Name = "SampleAssistant",
-    Instructions = "You are an expert in the field of Managed Services. Describe the benefits of Managed Services to a potential customer.",
+    Description = "Managed Services Assistant",
+    Name = "MS Assistant",
+    Instructions = "You are an expert in the field of Managed Services. Describe the benefits of Managed Services to a potential customer. Prepare an email with the details to the sender",
     EnableCodeInterpreter = true,
     EnableFileSearch = false,
     EnableJsonResponse = false,
@@ -68,7 +72,7 @@ OpenAIAssistantAgent agent =
 
 string threadId = await agent.CreateThreadAsync();
 
-await agent.AddChatMessageAsync(threadId, new ChatMessageContent(AuthorRole.User, "What is Managed Conference Room Service ? Given a room that accomodates 15 people, estimate the cost of service and show the results in a table"));
+await agent.AddChatMessageAsync(threadId, new ChatMessageContent(AuthorRole.User, " Send an email response of how much it would cost to book a Managed services room for 10 people. Ask for details if needed"));
 
 await foreach (ChatMessageContent message in agent.InvokeAsync(threadId))
 {
