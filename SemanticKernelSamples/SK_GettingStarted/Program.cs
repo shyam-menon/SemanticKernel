e8a5c78c0a4cc1687442282465f695c4aa93557b
Shyam Menon <shyam.menon@hp.com>
2024-08-17 07:32:48 +0530

    Ensuring predictability, refining, few shot prompting and handling unexpected requests

--- SemanticKernelSamples/SK_GettingStarted/Program.cs
+++ SemanticKernelSamples/SK_GettingStarted/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.SemanticKernel;
+﻿using Azure.Core;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
@@ -43,6 +44,10 @@ while ((userInput = Console.ReadLine()) != null)
     // Add user input
     history.AddUserMessage(userInput);
 
+    //Analyse the prompt
+    //await AnalysePrompt(kernel, userInput);
+
+    /////////////////////////////////////////////
 
     // Enable auto function calling
     OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
@@ -69,3 +74,60 @@ while ((userInput = Console.ReadLine()) != null)
     Console.Write("User > ");
 }
 
+static async Task AnalysePrompt(Kernel kernel, string? userInput)
+{
+    // prompt to send to AI
+    string prompt = $"What is the intent of this request? {userInput}";
+    Console.WriteLine("\n");
+    // Print the results
+    Console.WriteLine(await kernel.InvokePromptAsync(prompt));
+
+
+    //Ensuring predictability by ensuring the LLM only handles a discrete set of intents
+    string prompt2 = $@"What is the intent of this request? {userInput}
+   You can choose between GetInformation, MakeQuote, ReportAFault.";
+    Console.WriteLine("\n");
+    // Print the results
+    Console.WriteLine(await kernel.InvokePromptAsync(prompt2));
+
+    //Further refine the output by adding some structure to the prompt by using additional formatting
+    string prompt3 = @$"Instructions: What is the intent of this request? 
+            Choices: GetInformation, MakeQuote, ReportAFault.
+            Request: {userInput}
+            Intent:";
+    // Print the results
+    Console.WriteLine(await kernel.InvokePromptAsync(prompt3));
+
+    //Earlier examples were based on zero-shot prompting. You can use few shot prompting to provide more context to the AI
+    string prompt4 = @$"Instructions: What is the intent of this request?
+          Choices: GetInformation, MakeQuote, ReportAFault.
+          User Input: Can you tell me about Managed Services?
+          Intent: GetInformation
+          User Input: Can you tell me how to make a quote?
+          Intent: MakeQuote
+          User Input: {userInput}
+          Intent: ";
+    Console.WriteLine(await kernel.InvokePromptAsync(prompt4));
+
+
+    //Handling the unexpected
+    string prompt5 = @$"
+        Instructions: What is the intent of this request?
+        If you don't know the intent, don't guess; instead respond with ""Unknown"".
+        Choices: GetInformation, MakeQuote, ReportAFault.
+
+        User Input: Can you tell me about Managed Services?
+        Intent: GetInformation
+
+        User Input: Can you tell me how to make a quote?
+        Intent: MakeQuote
+
+        User Input: Can you tell me how to file a complaint?
+        Intent: ReportAFault
+
+        User Input: {userInput}
+        Intent: ";
+
+     Console.WriteLine(await kernel.InvokePromptAsync(prompt5));
+}
+             
